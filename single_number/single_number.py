69df3545e9e1029e413502e0d4781e383af7c7f8
Sherexmykes <sherexmykes@gmail.com>
2020-07-15 20:48:17 -0400

    product of all other numbers, sliding window

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,25 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # Your Code Here
+    #We need to initalize a null list for single integers
+    single_list = []
+    duplicate_list = []
 
-    pass
+    # go through all elements
+    for element in arr:
+        if element not in single_list:
+            single_list.append(element)
+        else:
+            duplicate_list.append(element)
+            single_list.remove(element)
+    return single_list[0]
 
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+    print({single_number(arr)})
+     # had to get rid of f"The odd-number-out is for the test to run
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,12 +5,15 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    resulted_array = []
+    for i in range(len(nums)-(k-1)):
+        end = i + k
+        resulted_array.append(max_num) 
 
-
+    return resulted_array
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print( {sliding_window_max(arr, k)})
